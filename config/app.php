6b9a60b5b5d86dfc8f15e374bd2d969906a92a83
joao <joao.p.ramos@gmail.com>
2015-06-27 11:52:55 +0100

    inicial commit

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'L2XeRDGrSXPq5Ure7D7US93ZauSDpC6l'),
 
     'cipher' => 'AES-256-CBC',
 
